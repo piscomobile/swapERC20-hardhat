8098d990e4fd8504e9485162d12d1e1ff6d442ff
piscomobile <julio.vera@piscomobile.com>
2021-11-01 22:19:39 -0300

    bicho

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -1,40 +1,67 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-import "./IDaiToken.sol"; 
+import "./IERC20.sol";
 
-//https://solidity-by-example.org/app/erc20/
+/*
+How to swap tokens
 
-contract Swapper {
+1. Alice has 100 tokens from AliceCoin, which is a ERC20 token.
+2. Bob has 100 tokens from BobCoin, which is also a ERC20 token.
+3. Alice and Bob wants to trade 10 AliceCoin for 20 BobCoin.
+4. Alice or Bob deploys TokenSwap
+5. Alice appproves TokenSwap to withdraw 10 tokens from AliceCoin
+6. Bob appproves TokenSwap to withdraw 20 tokens from BobCoin
+7. Alice or Bob calls TokenSwap.swap()
+8. Alice and Bob traded tokens successfully.
+*/
 
-    address private fromToken;
-    address private toToken;  
-    IDaiToken public daiToken;
+contract TokenSwap {
+    IERC20 public token1;
+    address public owner1;
+    uint public amount1;
+    IERC20 public token2;
+    address public owner2;
+    uint public amount2;
 
-
- constructor(address _fromToken ,address _toToken){
-
-     fromToken = _fromToken;
-     toToken = _toToken;
-
- }
-
-    function provide(uint _amount) external {
-
-
-    };
-
-
-    function swap () external {
-
-
+    constructor(
+        address _token1,
+        address _owner1,
+        uint _amount1,
+        address _token2,
+        address _owner2,
+        uint _amount2
+    ) {
+        token1 = IERC20(_token1);
+        owner1 = _owner1;
+        amount1 = _amount1;
+        token2 = IERC20(_token2);
+        owner2 = _owner2;
+        amount2 = _amount2;
     }
 
+    function swap() public {
+        require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
+        require(
+            token1.allowance(owner1, address(this)) >= amount1,
+            "Token 1 allowance too low"
+        );
+        require(
+            token2.allowance(owner2, address(this)) >= amount2,
+            "Token 2 allowance too low"
+        );
 
-    function getBalance(address _address)public view returns (uint ){
-          console.log(daiToken.balanceOf(_address));
-          return daiToken.balanceOf(_address);
+        _safeTransferFrom(token1, owner1, owner2, amount1);
+        _safeTransferFrom(token2, owner2, owner1, amount2);
     }
 
+    function _safeTransferFrom(
+        IERC20 token,
+        address sender,
+        address recipient,
+        uint amount
+    ) private {
+        bool sent = token.transferFrom(sender, recipient, amount);
+        require(sent, "Token transfer failed");
+    }
 }
---
+++ scripts/deploy-swap.js
@@ -0,0 +1,44 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+
+  
+  const TokenA = await hre.ethers.getContractFactory("TokenA");
+  const tokenA = await TokenA.deploy();
+  await tokenA.deployed();
+  
+  console.log("TokenA:", tokenA.address);
+
+  const TokenB = await hre.ethers.getContractFactory("TokenB");
+  const tokenB = await TokenB.deploy();
+  await tokenB.deployed();
+  console.log("TokenB:", tokenB.address);
+
+  // We get the contract to deploy
+  const Swap = await hre.ethers.getContractFactory("Swap2");
+  const swap = await Swap.deploy("0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea");
+  await swap.deployed();
+
+  console.log("Swap:", swap.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
