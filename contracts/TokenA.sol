b64d308b0cee2a4ee7b4e62667f2eb41de3da657
piscomobile <julio.vera@piscomobile.com>
2021-11-25 22:07:15 -0300

    talk to me

--- contracts/TokenA.sol
+++ contracts/TokenA.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 
 contract TokenA is ERC20, Ownable {
     constructor() ERC20("TokenA","TKA") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
+        console.log("inside Token A mint");
         _mint(to, amount);
     }
 }
--- contracts/TokenB.sol
+++ contracts/TokenB.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
 
 
 contract TokenB is ERC20, Ownable {
     constructor() ERC20("TokenB","TKB") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
+        console.log("inside Token B mint");
         _mint(to, amount);
     }
 }
